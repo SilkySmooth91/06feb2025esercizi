819dc0d497ec3e5ad04ffc05a08702f8566da7e7
SilkySmooth91 <lorenzo.olivieri13@gmail.com>
2025-02-08 17:28:51 +0100

    concluso

--- HW2/script.js
+++ HW2/script.js
@@ -1,11 +1,15 @@
 const spotifyEndpoint = "https://striveschool-api.herokuapp.com/api/deezer/search?q="
-
+const input = document.getElementById("searchField")
+const inputValue = input.value
 
 
 function search(query) {
     return fetch(spotifyEndpoint + query)
     .then(response => response.json())
-    .then(songs => renderSongs(songs))
+    .then(songs => {
+        renderSongs(songs)
+        return songs
+    })
     .catch(err => console.log(err))
 }
 
@@ -19,22 +23,22 @@ function renderSongs(songs) {
 
     songs.data.forEach(song => {
         const card = document.createElement("div")
-        card.classList.add("card", "mb-3", "bg-transparent", "col-2", "col-md-3", "border-0")
+        card.classList.add("card", "mb-3", "bg-transparent", "border-0")
 
         const img = document.createElement("img")
         img.src = song.album.cover_medium
         img.classList.add("card-img-top")
 
         const cardBody = document.createElement("div")
-        cardBody.classList.add("card-body")
+        cardBody.classList.add("card-body", "px-0")
 
         const cardTitle = document.createElement("h5")
-        cardTitle.classList.add("card-title")
-        cardTitle.textContent = song.title
+        cardTitle.classList.add("card-title", "fw-bold")
+        cardTitle.innerText = song.title
 
         const artist = document.createElement("p")
-        artist.classList.add("card-text")
-        artist.textContent = song.artist.name
+        artist.classList.add("card-text", "fw-light", "fs-small")
+        artist.innerText = song.artist.name
 
         cardBody.appendChild(cardTitle)
         cardBody.appendChild(artist)
@@ -46,7 +50,36 @@ function renderSongs(songs) {
     });
 }
 
+function albumList() {
+    const modalBody = document.querySelector(".modal-body")
+    modalBody.innerHTML=""
+
+    search(inputValue)
+    .then(songs => {
+        let addedAlbums = []
+
+        songs.data.forEach(song => {
+            const albumTitle = song.album.title
+
+            if (!addedAlbums.includes(albumTitle)) {
+                addedAlbums.push(albumTitle)
+                const albumP = document.createElement("p")
+                albumP.innerText = song.album.title + " - " + song.artist.name
+                albumP.classList.add("text-white")
+    
+                modalBody.appendChild(albumP)
+            }
+        } )
+    })
+    .catch(err => console.log(err))
+}
+
 document.getElementById("button-search").addEventListener("click", () => {
-    search("eminem")
+    const input = document.getElementById("searchField")
+    const inputValue = input.value
+    search(inputValue)
 })
 
+document.getElementById("list-button").addEventListener("click", () => {
+    albumList()
+})
